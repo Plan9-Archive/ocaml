78c3df814ae025467998b227bb890e00b1399b1d
spew <spew@localhost>
2018-05-23 21:32:30 -0400

    more files

---
+++ byterun/caml/s.h
@@ -0,0 +1,10 @@
+#define OCAML_OS_TYPE "Unix"
+#define OCAML_STDLIB_DIR "/usr/ocaml/lib"
+#define HAS_TIMES
+#define HAS_SOCKETS
+#define HAS_UNISTD
+#define HAS_OFF_T
+#define HAS_DIRENT
+#define HAS_GETCWD
+#define HAS_SELECT
+#define HAS_GETTIMEOFDAY
